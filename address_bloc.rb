f721012e756cbf66e15cb0d35df414e01cd54958
Arthur Philadelpho <arthur22rj@gmail.com>
2017-01-11 17:57:53 -0200

    Checkpoint 6 Complete

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
+# Connect to the menu_contoller.rb file.
+require_relative 'controllers/menu_controller.rb'
+# Create a new menu.
+menu = MenuController.new
+# Clear command line.
+system "clear"
 puts "Welcome to Address Bloc!"
+# Display our menu.
+menu.main_menu
